dea0c19223d5669f969ff7fdb119a958ec764b43
Ivan Smolyarov <i.a.smoliarov@gmail.com>
2025-02-23 12:25:23 +0300

    [2-working-with-code] Worked on scripts
    - Added script for logo (worked with position, rotation, scaling, updating data)
    - Added script for level (worked with another node, worked with array)

---
+++ scenes/logo.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+var pos: Vector2 = Vector2.ZERO # == Vector2(0, 0)
+const speed: float = 0.25
+var test_scale: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pos = Vector2(300, 200)
+	position = pos
+	
+	var test_rotation: int = 45
+	rotation_degrees = test_rotation
+	
+	test_scale = 2
+	scale = Vector2(test_scale, test_scale)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pos.x += speed
+	position = pos
+	
+	#test_scale += 0.01
+	#scale = Vector2(test_scale, test_scale)
